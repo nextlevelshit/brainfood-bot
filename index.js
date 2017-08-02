00b2a45aa284d93d4e1a8f02e0e9f5d1bcafa746
Michael Czechowski <mail@dailysh.it>
2017-08-02 17:15:56 +0200

    adding twitter status updater

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 // External libraries should be loaded at the beginning
 const TelegramBot = require('node-telegram-bot-api');
+const Twit = require('twit');
 // Local libraries and configurating local constants
 const config = require('./config');
 // Configurating our main piece of code
-const bot = new TelegramBot(config.token, {polling: true});
+const bot = new TelegramBot(config.telegram.token, {polling: true});
+const T = new Twit(config.twitter);
 
 bot.on('text', (msg) => {
-  if (msg.entities[0].type === 'url') {
-    console.log(`[${msg.chat.title}] ${msg.text}`);
-    bot.sendMessage(msg.chat.id, `Found link: ${msg.text}`);
+  if (msg.entities) {
+    if (msg.entities[0].type === 'url') {
+      let status = `[${msg.chat.title}] ${msg.text}`;
+
+      T.post('statuses/update', { status: status }, (error, data, response) => {
+        if (error) {
+          console.error(error); return;
+        }
+        console.log(status, data);
+      });
+    }
   }
 });
