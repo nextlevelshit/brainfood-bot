7fddbdf7c0194517fe1f45d03662f8bae0271e85
Michael Czechowski <mail@dailysh.it>
2017-08-02 16:14:43 +0200

    listeners for links running

--- index.js
+++ index.js
@@ -2,18 +2,12 @@
 const TelegramBot = require('node-telegram-bot-api');
 // Local libraries and configurating local constants
 const config = require('./config');
-const token = config.token;
 // Configurating our main piece of code
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(config.token, {polling: true});
 
-bot.on('message', (msg) => {
-  var hi = 'hi';
-  if (msg.text.toLowerCase().indexOf(hi) === 0) {
-    bot.sendMessage(msg.chat.id,'Hello dear user');
-  } 
-      
-  let bye = 'bye';
-  if (msg.text.toLowerCase().includes(bye)) {
-    bot.sendMessage(msg.chat.id, 'Hope to see you around again , Bye');
-  } 
+bot.on('text', (msg) => {
+  if (msg.entities[0].type === 'url') {
+    console.log(`[${msg.chat.title}] ${msg.text}`);
+    bot.sendMessage(msg.chat.id, `Found link: ${msg.text}`);
+  }
 });
