b638fb86fe348c626ce8590838cfb9887c3d21bd
Michael Czechowski <mail@dailysh.it>
2018-06-30 13:55:35 +0200

    yes

--- index.js
+++ index.js
@@ -1,31 +1,100 @@
-// External libraries should be loaded at the beginning
-const TelegramBot = require('node-telegram-bot-api');
-const Twit = require('twit');
-// Local libraries and configurating local constants
-const config = require('./config');
-// Configurating our main piece of code
-const bot = new TelegramBot(config.telegram.token, {polling: true});
-const T = new Twit(config.twitter);
+// External libraries
+const Telegraf = require('telegraf')
+const _ = require('lodash')
+const https = require('https')
+const cheerio = require('cheerio')
+// Local libraries and configurations
+const config = require('./config')
+const bot = new Telegraf(config.telegram.token)
 
-bot.on('text', (msg) => {
-      console.log(msg.chat.id);
-  if (msg.entities) {
-    if (msg.entities[0].type === 'url') {
-      let status = `[${msg.chat.title}] ${msg.text}`;
+let entries = []
+let connection = null
+let httpResponse = null
 
+bot.start((ctx) => {
+  setConnection(ctx)
+  startCrawler()
+  sendWelcome()
+})
 
-      T.post('statuses/update', { status: status }, (error, data, response) => {
-        if (error) {
-          console.error(error); return;
-        }
-        console.log(status, data);
-      });
-    }
+bot.startPolling()
+
+function startCrawler() {
+  console.log('[new user connected]')
+  console.log(connection.update.message.from)
+
+  crawl(config.url)
+
+  setTimeout(crawl, config.interval, config.url)
+}
+
+function sendWelcome() {
+  connection.replyWithMarkdown(`
+*Soeben hast du den WG-Gesucht Bot gestartet.*
+Sobald neue Inserate verfügbar sind, sende ich dir eine Nachricht. Viel Glück bei der Suche!
+  `)
+}
+
+function crawl(url) {
+  https.get(url, (res) => {
+    let data = ''
+
+    res.on('data', (chunk) => { data += chunk })
+    res.on('end', () => { checkNewEntries(data) });
+
+  }).on('error', (err) => {
+    console.error(`Error:${err.message}`)
+  });
+}
+
+function checkNewEntries(data) {
+  let newEntries = _.difference(parseEntries(data), entries)
+
+  if (!newEntries) {
+    console.warn('[no new entries]')
+    return
   }
-});
 
-T.stream('user').on('quoted_tweet', function (event) {
-  let status = `[${event.source.name}] ${event.target_object.text}`;
-  bot.sendMessage('-169241561', status);
-  console.log(status);
-});
+  newEntries.forEach(item => {
+    sendNewEntry(item)
+  })
+
+  entries = _.uniqBy([...newEntries, ...entries], 'url')
+}
+
+function parseEntries(content) {
+  const $ = cheerio.load(content);
+  let entries = [];
+
+  $('[id^=liste-details-ad]').not('.display-none').each(function(i, item) {
+    let result = {
+      url: config.urlPrefix + $(item).find('a.detailansicht').attr('href')
+    }
+    entries.push(result)
+  });
+
+  return entries
+}
+
+/**
+ * Sending new Entry to all followers
+ * 
+ * @param entry: {
+ *  price: number
+ *  disctrict: string
+ *  url: string
+ * }
+ */
+
+function sendNewEntry(entry) {
+  connection.reply(entry.url);
+}
+
+function httpRequest() {
+  return 
+}
+
+function setConnection(ctx) {
+  connection = ctx
+}
+
